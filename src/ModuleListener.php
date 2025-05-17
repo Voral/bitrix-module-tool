eeefddcf1a91a3179ebb9b94d413dccefbf8866d
Alexander Vorobyev <voral.adm@gmail.com>
2025-05-17 19:36:24 +0300

    chore: Настройка phpstan level 10

--- src/ModuleListener.php
+++ src/ModuleListener.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Voral\BitrixModuleTool;
 
+use http\Exception\RuntimeException;
 use Vasoft\VersionIncrement\Commits\CommitCollection;
 use Vasoft\VersionIncrement\Commits\FileModifyType;
 use Vasoft\VersionIncrement\Commits\ModifiedFile;
@@ -10,36 +13,68 @@ use Vasoft\VersionIncrement\Events\Event;
 use Vasoft\VersionIncrement\Config;
 use Vasoft\VersionIncrement\Exceptions\GitCommandException;
 use Vasoft\VersionIncrement\SemanticVersionUpdater;
+use Vasoft\VersionIncrement\Contract\EventListenerInterface;
 
-class ModuleListener implements \Vasoft\VersionIncrement\Contract\EventListenerInterface
+class ModuleListener implements EventListenerInterface
 {
     private readonly string $projectPath;
     private readonly string $sourcePath;
     private string $destinationPath;
 
+    /**
+     * @param Config               $config             Конфигурация version-increment
+     * @param string               $moduleId           Идентификатор модуля
+     * @param string               $sourcePath         Каталог с исходниками модуля
+     * @param string               $destinationPath    Путь для файлов обновлений
+     * @param string               $phpVersion         Версия PHP если необходим контроль в скрипте обновления
+     * @param array<string,string> $modulesVersion     Требующиеся модули и их версии
+     * @param array<string>        $excludeCommitTypes Типы коммитов не включаемые в файлы описания обновлений
+     * @param array<string>        $lang               Символьные коды языков для создания описания обновлений
+     * @param string               $includePhpFile     Путь у php файлу, код которого необходимо включить в скрипт обновления
+     */
     public function __construct(
         private readonly Config $config,
+        private readonly string $moduleId,
         string $sourcePath = 'last_version',
         string $destinationPath = 'updates',
-        private readonly string $moduleId = 'vasoft.core',
         private readonly string $phpVersion = '',
         private readonly array $modulesVersion = [],
         private readonly array $excludeCommitTypes = [],
         private readonly array $lang = ['ru', 'en'],
         private readonly string $includePhpFile = '',
     ) {
-        $this->projectPath = realpath(getenv('COMPOSER') ?: getcwd()) . DIRECTORY_SEPARATOR;
+        $this->projectPath = $this->getProjectPath();
         $this->sourcePath = $this->normalizePath($sourcePath);
         $this->destinationPath = $this->normalizePath($destinationPath);
     }
 
+    private function getProjectPath(): string
+    {
+        $composer = getenv('COMPOSER');
+        if (false !== $composer) {
+            $path = $composer;
+        } else {
+            $path = getcwd();
+            if (false === $path) {
+                throw new RuntimeException('Unable to determine current working directory');
+            }
+        }
+        $path = realpath($path);
+        if (false === $path) {
+            throw new RuntimeException('Unable to determine current working directory');
+        }
+
+        return $path . \DIRECTORY_SEPARATOR;
+    }
+
     private function normalizePath(string $path): string
     {
-        $path = rtrim($path, " \t\n\r\0\x0B\/");
-        if (!str_starts_with($path, DIRECTORY_SEPARATOR)) {
+        $path = rtrim($path, " \t\n\r\0\x0B\\/");
+        if (!str_starts_with($path, \DIRECTORY_SEPARATOR)) {
             $path = $this->projectPath . $path;
         }
-        return $path . DIRECTORY_SEPARATOR;
+
+        return $path . \DIRECTORY_SEPARATOR;
     }
 
     private function updateModuleVersion(string $version): void
@@ -48,44 +83,42 @@ class ModuleListener implements \Vasoft\VersionIncrement\Contract\EventListenerI
         $date = date('Y-m-d');
         if (file_exists($fileName)) {
             $content = file_get_contents($fileName);
-            $content = preg_replace(
-                '/[\'"]VERSION[\'"]\s*=>\s*[\'"](.*?)[\'"]/',
-                '\'VERSION\' => \'' . $version . '\'',
-                $content
-            );
-            $content = preg_replace(
-                '/[\'"]VERSION_DATE[\'"]\s*=>\s*[\'"](.*?)[\'"]/',
-                '\'VERSION_DATE\' => \'' . $date . '\'',
-                $content
-            );
+            if ($content) {
+                $content = (string) preg_replace(
+                    '/[\'"]VERSION[\'"]\s*=>\s*[\'"](.*?)[\'"]/',
+                    '\'VERSION\' => \'' . $version . '\'',
+                    $content,
+                );
+                $content = (string) preg_replace(
+                    '/[\'"]VERSION_DATE[\'"]\s*=>\s*[\'"](.*?)[\'"]/',
+                    '\'VERSION_DATE\' => \'' . $date . '\'',
+                    $content,
+                );
+            }
         } else {
             $content = <<<PHP
-<?php
+                <?php
 
-declare(strict_types=1);
+                declare(strict_types=1);
 
-\$arModuleVersion = [
-    'VERSION' => '$version',
-    'VERSION_DATE' => '$date',
-];
+                \$arModuleVersion = [
+                    'VERSION' => '{$version}',
+                    'VERSION_DATE' => '{$date}',
+                ];
 
-PHP;
+                PHP;
         }
         file_put_contents($fileName, $content);
     }
 
-
     public function handle(Event $event): void
     {
-        echo __LINE__,' > ',$this->includePhpFile,PHP_EOL;
         $lastTag = $event->getData(SemanticVersionUpdater::LAST_VERSION_TAG);
-        echo __LINE__,' > ',$this->includePhpFile,PHP_EOL;
-        if (empty($lastTag)) {
+        if (empty($lastTag) || !is_string($lastTag)) {
             exit;
         }
-        echo __LINE__,' > ',$this->includePhpFile,PHP_EOL;
         $this->updateModuleVersion($event->version);
-        $this->destinationPath .= $event->version . DIRECTORY_SEPARATOR;
+        $this->destinationPath .= $event->version . \DIRECTORY_SEPARATOR;
         [$copy, $remove] = $this->getModifiedFiles($lastTag);
         $copy[$this->destinationPath . 'install/version.php'] = $this->sourcePath . 'install/version.php';
         $this->copyFilesWithStructure($copy);
@@ -98,7 +131,6 @@ PHP;
         }
     }
 
-
     private function writeDescription(CommitCollection $commitCollection): void
     {
         if (empty($this->lang)) {
@@ -108,7 +140,7 @@ PHP;
         /** @var Section $section */
         foreach ($commitCollection as $section) {
             foreach ($section->getCommits() as $commit) {
-                if (in_array($commit->type, $this->excludeCommitTypes)) {
+                if (in_array($commit->type, $this->excludeCommitTypes, true)) {
                     continue;
                 }
                 $content .= '- ' . $commit->comment . PHP_EOL;
@@ -135,27 +167,31 @@ PHP;
         $this->config->getVcsExecutor()->addFile($fileName);
     }
 
+    /**
+     * @param array<string> $remove
+     *
+     * @throws GitCommandException
+     */
     private function writeUpdater(array $remove, string $version): void
     {
-        echo __LINE__,' > ',$this->includePhpFile,PHP_EOL;
         $delete = $this->getRemoveFiles($remove);
         $copyDirectories = $this->getCopyDirectories();
         $versionPhp = $this->controlPhpVersion();
 
         $content = <<<PHP
-<?php
+            <?php
 
-declare(strict_types=1);
+            declare(strict_types=1);
 
-/**
-* @var CUpdater \$updater
-*/
+            /**
+            * @var CUpdater \$updater
+            */
 
-$versionPhp
-$delete
-$copyDirectories
+            {$versionPhp}
+            {$delete}
+            {$copyDirectories}
 
-PHP;
+            PHP;
         $fileName = $this->destinationPath . 'updater' . $version . '.php';
         file_put_contents($fileName, $content);
         $this->config->getVcsExecutor()->addFile($fileName);
@@ -163,17 +199,18 @@ PHP;
 
     private function controlPhpVersion(): string
     {
-        if ($this->phpVersion === '') {
+        if ('' === $this->phpVersion) {
             return '';
         }
+
         return <<<PHP
 
-if (!version_compare(PHP_VERSION, '{$this->phpVersion}', '>=')) {
-    \$updater->errorMessage[] = 'Requires PHP version {$this->phpVersion} and higher';
-    return;
-}
+            if (!version_compare(PHP_VERSION, '{$this->phpVersion}', '>=')) {
+                \$updater->errorMessage[] = 'Requires PHP version {$this->phpVersion} and higher';
+                return;
+            }
 
-PHP;
+            PHP;
     }
 
     private function getCopyDirectories(): string
@@ -188,8 +225,9 @@ PHP;
         $adminExists = false;
         foreach ($iterator as $fileInfo) {
             if ($fileInfo->isDir() && !$fileInfo->isDot()) {
-                if ($fileInfo->getFilename() === 'admin') {
+                if ('admin' === $fileInfo->getFilename()) {
                     $adminExists = true;
+
                     continue;
                 }
                 $commands[] = "    \$updater->CopyFiles('install/{$fileInfo->getFilename()}', '{$fileInfo->getFilename()}');";
@@ -199,25 +237,28 @@ PHP;
         if ($adminExists) {
             $contents .= $this->getCopyAdminPages();
         }
-        if ($this->includePhpFile !== '' && file_exists($this->includePhpFile)) {
+        if ('' !== $this->includePhpFile && file_exists($this->includePhpFile)) {
             $code = file_get_contents($this->includePhpFile);
-            $code = trim(preg_replace('/<\?(php)?/', '', $code));
-            $contents .= PHP_EOL.PHP_EOL.$code;
+            if ($code) {
+                $code = trim((string) preg_replace('/<\?(php)?/', '', $code));
+                $contents .= PHP_EOL . PHP_EOL . $code;
+            }
         }
 
         return <<<PHP
 
-if(IsModuleInstalled('{$this->moduleId}')){
-$contents
-}
+            if(IsModuleInstalled('{$this->moduleId}')){
+            {$contents}
+            }
 
-PHP;
+            PHP;
     }
 
     private function formatVarExport(mixed $value): string
     {
         $export = var_export($value, true);
-        return preg_replace('/(?:\r\n|\r|\n)/', '$0    ', $export);
+
+        return (string) preg_replace('/(?:\r\n|\r|\n)/', '$0    ', $export);
     }
 
     private function getCopyAdminPages(): string
@@ -230,21 +271,25 @@ PHP;
                 $files[] = $fileInfo->getFilename();
             }
         }
-        $fileList = $this->formatVarExport($files, true);
+        $fileList = $this->formatVarExport($files);
         $modulePrefix = str_replace('.', '_', $this->moduleId) . '_';
         $path = "/bitrix/modules/{$this->moduleId}/install/admin/";
+
         return <<<PHP
 
-    \$adminFiles = $fileList;
-    foreach (\$adminFiles as \$file) {
-        file_put_contents(
-            \$_SERVER["DOCUMENT_ROOT"].\$updater->kernelPath . '/admin/$modulePrefix' . \$file,
-            '<?php require(\$_SERVER["DOCUMENT_ROOT"].\'{$path}'.\$file.'\');',
-        );
-    }
-PHP;
+                \$adminFiles = {$fileList};
+                foreach (\$adminFiles as \$file) {
+                    file_put_contents(
+                        \$_SERVER["DOCUMENT_ROOT"].\$updater->kernelPath . '/admin/{$modulePrefix}' . \$file,
+                        '<?php require(\$_SERVER["DOCUMENT_ROOT"].\\'{$path}'.\$file.'\\');',
+                    );
+                }
+            PHP;
     }
 
+    /**
+     * @param array<string> $remove
+     */
     private function getRemoveFiles(array $remove): string
     {
         if (empty($remove)) {
@@ -254,18 +299,22 @@ PHP;
             $file = str_replace($this->sourcePath, '/modules/' . $this->moduleId . '/', $file);
         }
         $delete = $this->formatVarExport($remove);
+
         return <<<PHP
-if(\$updater->CanUpdateKernel()) {
-// Отредактируйте пути в массиве удаляемых файлов
-    \$filesForDelete = $delete;
-    foreach (\$filesForDelete as \$file) {
-        CUpdateSystem::DeleteDirFilesEx(\$_SERVER["DOCUMENT_ROOT"] . \$updater->kernelPath . "/" . \$file);
-    }
-}
+            if(\$updater->CanUpdateKernel()) {
+            // Отредактируйте пути в массиве удаляемых файлов
+                \$filesForDelete = {$delete};
+                foreach (\$filesForDelete as \$file) {
+                    CUpdateSystem::DeleteDirFilesEx(\$_SERVER["DOCUMENT_ROOT"] . \$updater->kernelPath . "/" . \$file);
+                }
+            }
 
-PHP;
+            PHP;
     }
 
+    /**
+     * @param array<string> $files
+     */
     private function copyFilesWithStructure(array $files): void
     {
         foreach ($files as $file) {
@@ -281,10 +330,9 @@ PHP;
         }
     }
 
-
     /**
-     * @param string $lastTag
-     * @return array
+     * @return array<array<string>>
+     *
      * @throws GitCommandException
      */
     private function getModifiedFiles(string $lastTag): array
@@ -294,9 +342,15 @@ PHP;
         $result = array_reduce($files, [$this, 'reduceFiles'], $result);
         $copy = array_keys($result['copy']);
         $remove = array_keys($result['remove']);
+
         return [$this->normalizeFiles($copy), $this->normalizeFiles($remove)];
     }
 
+    /**
+     * @param array<string,array<string,true>> $result
+     *
+     * @return array<string,array<string,true>>
+     */
     private function reduceFiles(array $result, ModifiedFile $file): array
     {
         switch ($file->type) {
@@ -320,9 +374,14 @@ PHP;
                 $result['copy'][$file->destination] = true;
                 break;
         }
+
         return $result;
     }
 
+    /**
+     * @param string             $path   Путь файлу
+     * @param array<string,true> $result Агрегатор измененных файлов
+     */
     private function unsetExists(string $path, array &$result): void
     {
         if (isset($result[$path])) {
@@ -330,12 +389,18 @@ PHP;
         }
     }
 
+    /**
+     * @param array<string> $files Массив файлов
+     *
+     * @return array<string>
+     */
     private function normalizeFiles(array $files): array
     {
         $files = array_unique($files);
         foreach ($files as &$file) {
             $file = $this->projectPath . $file;
         }
+
         return $files;
     }
 }
